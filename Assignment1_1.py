f041912ec714f61157ab8f91a269eacbdc62f6ab
ravi-nder1987 <ravindersuryavanshi85@gmail.com>
2025-05-23 02:26:05 +0530

    Add files via upload
    Assignment1

---
+++ Assignment1_1.py
@@ -0,0 +1,14 @@
+#Program for basic operation
+
+num1=int(input("Enter the first Number:"))
+num2=int(input("Enter the Second Number:"))
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1/num2
+
+print('Addition:',add)
+print('Subtraction:',sub)
+print('Multiplication:',mul)
+print('Division:',div)
---
+++ Assignment1_2.py
@@ -0,0 +1,7 @@
+#Assignment1 Task2
+
+fname = input('Enter your First Name :')
+lname = input('Enter your Last Name :')
+fullName = fname + ' ' + lname
+print("Hello, "+fullName + "! Welcome to the Python Program")
+
